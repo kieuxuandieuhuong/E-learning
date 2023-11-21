e3535e27e2ba6499a5308d25c1e89d5e69916eec
kieuxuandieuhuong <115691878+kieuxuandieuhuong@users.noreply.github.com>
2023-11-21 23:22:11 +0700

    first commit

---
+++ src/Components/Course/Course.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Course.css";
+
+const Course = () => {
+  return (
+    <div>Course</div>
+  )
+}
+
+export default Course
---
+++ src/Components/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home/Home";
+import Testimonials from "./Testimonials/Testimonials";
+import Infor from "./Infor/Infor";
+import Intro from "./Intro/Intro";
+import Footer from "./Footer/Footer";
+import Course from "./Course/Course";
+
+export {Home, Intro, Infor, Course, Testimonials, Footer };
